2462eb702ed20d988b47003001ca5f7671899e85
FabioM <130793077+MenAtwork-FMP@users.noreply.github.com>
2024-03-05 17:06:48 -0600

    Update to Runrepo and Utility modules

---
+++ 00_Runrepo.py
@@ -0,0 +1,17 @@
+############################################
+# Main Code File to run other subprocesess #
+############################################
+
+#import libraries
+import subprocess
+
+
+##########################
+# Subprocesses execution #
+##########################
+
+utility_file_path = "./01_Utility.py"
+
+
+subprocess.run(["python", utility_file_path])
+
--- 01_Utility.py
+++ 01_Utility.py
@@ -4,6 +4,35 @@
 
 import yaml
 import os
+import logging
+
+
+#####################
+# Set Log Messenger #
+#####################
+
+logmessenger = logging.getLogger(__name__)
+logmessenger.setLevel(logging.INFO)
+
+logfolder = 'Logs'
+logfilename = 'Logs.log'
+logfilenamepath = os.path.join(logfolder,logfilename)
+
+if not os.path.exists(logfolder):
+        os.makedirs(logfolder)
+else:
+    if os.path.exists(logfilenamepath):
+        with open(logfilenamepath, "w") as file:
+            pass
+    else:
+        open(logfilenamepath, "w").close()
+
+file_handler = logging.FileHandler(logfilenamepath)
+formatter = logging.Formatter('%(asctime)s : %(levelname)s : %(filename)s : %(message)s')
+file_handler.setFormatter(formatter)
+logmessenger.addHandler(file_handler)
+
+logmessenger.info('Log Messenger set up finalized')
 
 
 ###################
@@ -12,9 +41,10 @@ import os
 
 class Utility:
 
-###Create INIT file
-    def __init__(self, params_path='params.yaml') -> None:
-        self.params_path = params_path
+###Create INIT file for parameters.yaml
+    def __init__(self, parameters_path='parameters.yaml') -> None:
+        self.parameters_path = parameters_path
+    logmessenger.info('Yaml INIT method created')
 
 ####Create method to create folders if they do not exist.
     def create_folder(self, folder_name):
@@ -24,8 +54,9 @@ class Utility:
 
         except Exception as e:
             raise e
+    logmessenger.info('Create Folder method defined')        
 
-###Creat method to read parameters
+###Create method to read parameters
     def read_parameters(self):
         try:
             with open(self.params_path, 'r') as params_file:
@@ -36,3 +67,4 @@ class Utility:
 
         else:
             return params
+    logmessenger.info('Read parameters method defined')        
